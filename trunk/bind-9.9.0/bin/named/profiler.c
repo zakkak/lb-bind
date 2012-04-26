fc96a3e35a673f8c5358d0317ce0f0c50e9a8292
FiVoSz@gmail.com <FiVoSz@gmail.com@66c83ea1-4d1a-0db5-e527-3589af75fc69>
2012-04-26 19:08:28 +0000

    Profiler progress, look the TODO in profiler_poll_workers

--- trunk/bind-9.9.0/bin/named/profiler.c
+++ trunk/bind-9.9.0/bin/named/profiler.c
@@ -4,31 +4,58 @@
  * Author: zakkak@csd.uoc.gr
  */
 
+// UPDATE_INTERVAL in seconds
+// TODO set TTL the same
+#define UPDATE_INTERVAL 60
+
+// assert( (CPU+IO+NET)==1.0f );
+#define CPU 0.5f
+#define IO  0.25f
+#define NET  0.25f
+#define CALC_LOAD(x) (x->cpu_load*CPU + x->io_load*IO + x->net_load*NET)
+
 typedef struct a_node {
-  isc_sockaddr_t sockaddr;      // worker's sockaddr
+//   isc_sockaddr_t sa;      // worker's sockaddr
+  dns_adbnamehook_t *nh;
   uint8_t cpu_load, io_load, net_load;  // Load percentages
 } a_node_t;
 
-typedef struct ht_node_v {
-  dns_adbnamehooklist_t orig_addresses;
+// typedef struct ht_node_v {
+//   a_node_t *addr_stats[LWRES_MAX_ADDRS];
+//   int naddrs;
+// } ht_node_v_t;
+// 
+// typedef struct ht_node {
+//   dns_name_t key;
+//   ht_node_v value;
+// } ht_node_t;
+// 
+// ht_hashtable_t ht_g;                 // mapping names to ht_nodes
+
+typedef struct node {
+  dns_name_t key;
   a_node_t *addr_stats[LWRES_MAX_ADDRS];
-} ht_node_v_t;
+  int naddrs;
+  struct node *next;
+} node_t;
 
-typedef struct ht_node {
-  char *key;
-  ht_node_v value;
-} ht_node_t;
+// typedef struct list {
+//   node_t* head;
+// } list_t;
 
-hashtable ht_g;                 // mapping names to ht_nodes
+node_t *list_g;
+
+void profiler_update_addrs();
 
 void profiler_init()
 {
+  int bucket;
   isc_result_t result;
-  dns_adb_t *adb;
-  unsigned int options;
-  dns_adbnamehook_t *namehook;
-  dns_adbentry_t *entry;
   dns_view_t *view;
+
+  // For our book keeping
+  node_t *value;
+  //////////////////////////////////////////////////////////////////////////////
 
   // For the rbt iterator
   dns_name_t foundname, *origin;
@@ -42,12 +69,13 @@ void profiler_init()
   origin = dns_fixedname_name(&fixedorigin);
   //////////////////////////////////////////////////////////////////////////////
 
+  dns_adb_t *adb;
+  dns_adbnamehook_t *namehook;
+  dns_adbentry_t *entry;
 
-  options = 0;
-  options |= DNS_ADBFIND_WANTEVENT;
-  options |= DNS_ADBFIND_RETURNLAME;
-  // ZAKKAK not supporting IPv6
-  options |= DNS_ADBFIND_INET;
+  list_g = NULL;
+
+  assert((CPU + IO + NET) == 1.0f);
 
   // Go through all views and initialize the hashtable
   for (view = ISC_LIST_HEAD(ns_g_server->viewlist); view != NULL; view = ISC_LIST_NEXT(view, link)) {
@@ -67,6 +95,12 @@ void profiler_init()
       for (;;) {
         if (result == ISC_R_SUCCESS || result == DNS_R_NEWORIGIN) {
 
+          value = (node_t *) malloc(sizeof(node_t));
+          dns_name_clone(foundname, value->key);
+          value->naddrs = 0;
+          value->next = list_g;
+          list_g = value;
+
           namehook = ISC_LIST_HEAD(foundname->v4);
           // iter through addresses
           while (namehook != NULL) {
@@ -74,13 +108,15 @@ void profiler_init()
             bucket = entry->lock_bucket;
             LOCK(&adb->entrylocks[bucket]);
 
-            entry->sockaddr;
+            value->addr_stats[value->naddrs] = (a_node_t *) malloc(sizeof(a_node_t));
+            memset(value->addr_stats[value->naddrs], 0, (sizeof(a_node_t)));
+//             memcpy(value->addr_stats[value->naddrs++]->sa, entry->sockaddr, sizeof(isc_sockaddr_t);
+            value->addr_stats[value->naddrs++]->nh = namehook;
 
             UNLOCK(&adb->entrylocks[bucket]);
             bucket = DNS_ADB_INVALIDBUCKET;
             namehook = ISC_LIST_NEXT(namehook, plink);
           }
-
         } else {
           if (result != ISC_R_NOMORE)
             printf("UNEXEPCTED ITERATION ERROR: %s", dns_result_totext(result));
@@ -94,93 +130,68 @@ void profiler_init()
     RWUNLOCK(&zonetable->rwlock, isc_rwlocktype_read);
   }
 
-#if 0
-  dns_zone_t **zonep dns_fixedname_t name;
-  dns_adbname_t *adbname;
-  isc_buffer_t buf;
-  dns_rdataclass_t rdclass;
-
-  isc_buffer_init(&buf, zonename, strlen(zonename));
-  isc_buffer_add(&buf, strlen(zonename));
-  dns_fixedname_init(&name);
-  result = dns_name_fromtext(dns_fixedname_name(&name), &buf, dns_rootname, 0, NULL);
-  if (result != ISC_R_SUCCESS)
-    goto fail1;
-
-  // what's the class?
-  rdclass = dns_rdataclass_in;
-
-  result = dns_viewlist_findzone(&ns_g_server->viewlist, dns_fixedname_name(&name), ISC_TRUE, rdclass, zonep);
-
-  /* Partial match? */
-  if (result != ISC_R_SUCCESS && *zonep != NULL)
-    dns_zone_detach(zonep);
-  if (result == DNS_R_PARTIALMATCH)
-    result = ISC_R_NOTFOUND;
-
-  if (result == ISC_R_SUCCESS) {
-    adb = zonep->view->adb;
-
-    /*
-     * Try to see if we know anything about this name at all.
-     */
-    bucket = DNS_ADB_INVALIDBUCKET;
-    adbname = find_name_and_lock(adb, name, options, &bucket);
-    INSIST(bucket != DNS_ADB_INVALIDBUCKET);
-    if (adb->name_sd[bucket]) {
-      DP(DEF_LEVEL, "named_profiler: returning ISC_R_SHUTTINGDOWN");
-      result = ISC_R_SHUTTINGDOWN;
-      goto out;
-    }
-
-    /*
-     * Nothing found.  Allocate a new adbname structure for this name.
-     */
-    if (adbname == NULL) {
-      // ZAKKAK it would be nice to never come here
-      /*
-       * See if there is any stale name at the end of list, and purge
-       * it if so.
-       */
-      check_stale_name(adb, bucket, now);
-
-      adbname = new_adbname(adb, name);
-      if (adbname == NULL) {
-        result = ISC_R_NOMEMORY;
-        goto out;
-      }
-      link_name(adb, bucket, adbname);
-    } else {
-      /* Move this name forward in the LRU list */
-      ISC_LIST_UNLINK(adb->names[bucket], adbname, plink);
-      ISC_LIST_PREPEND(adb->names[bucket], adbname, plink);
-    }
-    adbname->last_used = now;
-
-    // iter through addresses
-    namehook = ISC_LIST_HEAD(name->v4);
-    while (namehook != NULL) {
-      entry = namehook->entry;
-      bucket = entry->lock_bucket;
-      LOCK(&adb->entrylocks[bucket]);
-
-      entry->sockaddr;
-
-      UNLOCK(&adb->entrylocks[bucket]);
-      bucket = DNS_ADB_INVALIDBUCKET;
-      namehook = ISC_LIST_NEXT(namehook, plink);
-    }
-  }
-
-fail1:
-  return (result);
-#endif
-
+  profiler_update_addrs();
 }
 
-
-// LOCK list before sorting
-void profiler_sort_addrs()
+void profiler_poll_workers(node_t * cur)
 {
+  int i;
+  a_node_t *tmp;
 
+  for (i = 0; i < cur->naddrs; ++i) {
+    tmp = cur->addr_stats[i];
+
+    //TODO here do the communication and store the results
+    // we can open and close the connection for each zone or keep it live for
+    // the whole session
+//     tmp->cpu_load = ;
+//     tmp->io_load = ;
+//     tmp->net_load = ;
+  }
+}
+
+int cmp(const void *v_a, const void *v_b)
+{
+  a_node_t *a, *b;
+  int s_a, s_b;
+
+  a = (a_node_t *) v_a;
+  b = (a_node_t *) v_b;
+
+  s_a = CALC_LOAD(a);
+  s_b = CALC_LOAD(b);
+
+  return s_a - s_b;
+}
+
+void profiler_update_addrs()
+{
+  node_t *current = list_g;
+  int i, bucket;
+  dns_adbnamehook_t *tmp;
+  dns_adbnamehooklist_t *list;
+
+  while (1) {
+    sleep(UPDATE_INTERVAL);
+
+    while (current) {
+
+      profiler_poll_workers(current);
+      qsort(current->addr_stats, current->naddrs, sizeof(a_node_t), cmp);
+
+
+      // LOCK list before sorting
+      bucket = current->key->lock_bucket;
+      LOCK(&current->key->adb->namelocks[bucket]);
+      list = &(current->key->v4);
+      ISC_LIST_INIT(*list);
+      for (i = 0; i < cur->naddrs; ++i) {
+        tmp = current->addr_stats[i]->nh;
+        ISC_LIST_APPEND(*list, tmp, plink);
+      }
+      UNLOCK(&current->key->adb->namelocks[bucket]);
+
+      current = list_g->next;
+    }
+  }
 }
