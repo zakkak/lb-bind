9f8d1d1e9b059b8cccb371aec60921d2e397636c
Foivos <zakkak@users.noreply.github.com>
2012-05-01 21:14:23 +0000

    Adding bind config files. Adding profiler.h

---
+++ bind-9.9.0/bin/named/include/named/profiler.h
@@ -0,0 +1,14 @@
+/*
+ * File: profiler.h
+ *
+ * Authors:
+ *  zakkak@csd.uoc.gr
+ *  hassapis@csd.uoc.gr
+ */
+
+#ifndef NAMED_PROFILER_H
+#define NAMED_PROFILER_H 1
+
+void ns_profiler_init();
+
+#endif //NAMED_PROFILER_H
