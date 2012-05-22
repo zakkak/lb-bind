4407d5887b6c1b10bb84be75f00f4e31ef280c63
k4s4s.heavener@gmail.com <k4s4s.heavener@gmail.com@66c83ea1-4d1a-0db5-e527-3589af75fc69>
2012-05-22 22:06:43 +0000

    * updated to latest benchmark.rb

--- trunk/benchmark/benchmark.rb
+++ trunk/benchmark/benchmark.rb
@@ -10,23 +10,38 @@ def range (min, max)
 end
 
 min = 0
-max = 100
+max = 10
 
 #lookup address
-names = Array["www.youtube.net"] 
+names = Array["www.hahakios.net"] 
+
+res = Dnsruby::Resolver.new
+res.do_caching = false 
 
 while true do
-	sleep(1)
+	sleep(0.5)
 
 	# Use the system configured nameservers to run a query
 	#names.each_index { | index | puts index }
-	res = Dnsruby::Resolver.new	
-	#ret = res.query("www.hahakios.net")
-	#puts ret.answer
-	ip = Dnsruby::Resolv.getaddress(names[range(0,0).to_i])
+	#res = Dnsruby::Resolver.new
+	#res.do_caching = false	
+	begin
+		ret = res.query(names[range(0,0).to_i]);
+	rescue Dnsruby::ResolvTimeout
+		puts "log: failed to get dns response"
+		next
+	end
+	#puts "*** ***"
+	#puts ret.answer.first
+	ip = ret.answer.first.rdata_to_string
+	#ret.answer.each { |entry|puts entry }
+	#puts ret.answer.first
+	#resolv = Dnsruby::DNS.new
+	#resolv.do_caching = false;
+	#ip = resolv.getaddress(names[range(0,0).to_i])
   #p Dnsruby::Resolv.getname("210.251.121.214")
-
-	clientSession = TCPSocket.new( "localhost", 2113 )
+	#ip = 1;
+	clientSession = TCPSocket.new( "192.168.1.73", 2113 )
 	puts "log: starting connection"
 	puts "log: sending request to worker:#{ip}"
 	#generate dummy loads
